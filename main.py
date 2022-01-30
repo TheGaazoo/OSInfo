77f6dc08f8d640d588eae93510992c7da2524c29
gaazoo <gaazoo@yandex.ru>
2022-01-30 19:48:03 +0700

    OS Info 2

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-import platform as pl
-
-
-def os_info():
-    profile = [
-        'architecture',
-        'linux_distribution',
-        'mac_ver',
-        'machine',
-        'node',
-        'platform',
-        'processor',
-        'python_build',
-        'python_compiler',
-        'python_version',
-        'release',
-        'system',
-        'uname',
-        'version',
-    ]
-    for key in profile:
-        if hasattr(pl, key):
-            print(key + ": " + str(getattr(pl, key)()))
-
-
-if __name__ == '__main__':
-    os_info()
+import platform
+import sys
+info = 'OS info is \n{}\n\nPython version is {} {}'.format(
+platform.uname(), sys.version, platform.architecture(), platform.version())
+print(info)
